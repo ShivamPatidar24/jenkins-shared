a3ef197500591837fc9cbfc457717aa17d14cd9b
Cvam7 <108720572+ShivamPatidar24@users.noreply.github.com>
2026-03-25 16:40:33 +0530

    Add echo statement for push in pubpush hub

--- vars/push.groovy
+++ vars/push.groovy
@@ -1,4 +1,5 @@
 def call(String Project, String ImageTag, String dockerhubuser){
+  echo "push in pub"
   withCredentials([usernamePassword(credentialsId: 'dockerhubcred', passwordVariable: 'dockerhubpass', usernameVariable: 'dockerhubuser')]) {
       sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
   }
